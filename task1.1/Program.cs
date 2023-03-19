d12175be481c33875e2ce53ed2175041b4ed9b1d
nikita <shtukolimp@mail.ru>
2023-03-19 20:04:38 +0300

    ещё раз первое задание

---
+++ task1.1/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("введите первое число");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("введите второе число");
+int b = int.Parse(Console.ReadLine()!);
+int max = 0;
+int min = 0;
+
+if (a > b)
+    {max = a;
+    min = b;}
+ else
+ {  max = b;
+    min = a;} 
+Console.Write("минимальное число: ");    
+Console.WriteLine(min);
+Console.Write("максимальное число: ");
+Console.WriteLine(max);
